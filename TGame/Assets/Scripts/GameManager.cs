107a9c7090aad9f5497130130b130a9f3ae46603
webhead3893 <71961459+webhead3893@users.noreply.github.com>
2020-12-01 23:19:22 +0800

    files

---
+++ TGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel(){
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame(){
+        if(gameHasEnded == false){
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    public void EndGameNoD(){
+        if(gameHasEnded == false){
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", 0f);
+        }
+    }
+
+    void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ TGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sideForce = 500f;
+
+    // Update is called once per frame
+    // FixedUpdate for physics stuff
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);   //add a z-axis force on the player
+
+        if(Input.GetKey("right")){
+            rb.AddForce(sideForce, 0, 0 * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if(Input.GetKey("left")){
+            rb.AddForce(-sideForce, 0, 0 * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if(rb.position.y < -1f){
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
