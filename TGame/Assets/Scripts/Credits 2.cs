db5e75ca98c321feed1bb03d280ebaac78329064
webhead3893 <71961459+webhead3893@users.noreply.github.com>
2020-12-17 15:38:09 +0800

    changed size of collider to avoid game over bug

---
+++ TGame/Assets/Scripts/Credits 2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    public void Quit(){
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        Debug.Log("Restart");
+        SceneManager.LoadScene(1);
+    }
+}
